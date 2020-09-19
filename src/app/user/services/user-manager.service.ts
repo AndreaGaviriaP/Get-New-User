38a70be68287b55377df0eb6aa28fbe60c4a5105
Andrea Gavira <andrea.gaviria@globant.com>
2020-09-19 18:20:00 -0500

    adding project

---
+++ src/app/user/services/user-manager.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, observable } from 'rxjs';
+
+//Models
+import { IUserResponse } from '../models/user'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserManagerService {
+
+  constructor(private http:HttpClient) { }
+
+   getRandomName(): Observable<IUserResponse> {
+     return this.http.get<IUserResponse>('https://randomuser.me/api');
+     } 
+   }
+
+ 
